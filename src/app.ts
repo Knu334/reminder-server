d590d4bae7efa06bccc875ba1beed49d26e8cff4
Knu334 <who.is.fuu@gmail.com>
2025-11-03 04:09:19 +0900

    証明書更新時に再読み込みする処理を追加

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import helmet from "helmet";
 import fs from "fs";
+import path from "path";
 import https from "https";
 
 import indexRouter from "@/router/index";
@@ -41,12 +42,42 @@ app.use((err: HttpError, req: Request, res: Response, _next: NextFunction) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  const options = {
-    key: fs.readFileSync(process.env.SSL_KEY || ""),
-    cert: fs.readFileSync(process.env.SSL_CERT || ""),
+  let server: https.Server;
+  const certPath = process.env.CERT_PATH || "";
+  const loadCertificates = () => {
+    return {
+      key: fs.readFileSync(path.join(certPath, "privkey.pem")),
+      cert: fs.readFileSync(path.join(certPath, "cert.pem")),
+    };
   };
-  const server = https.createServer(options, app);
-  server.listen(Number(process.env.PORT) || 3000);
+  const startServer = () => {
+    const credentials = loadCertificates();
+
+    if (server) {
+      server.close(() => {
+        console.log("旧サーバーをクローズしました");
+        server = https.createServer(credentials, app);
+        server.listen(Number(process.env.PORT || 3000), () => {
+          console.log("新しい証明書でサーバーを再起動しました");
+        });
+      });
+    } else {
+      server = https.createServer(credentials, app);
+      server.listen(Number(process.env.PORT || 3000), () => {
+        console.log("サーバーを起動しました");
+      });
+    }
+  };
+
+  // 証明書ファイルを監視
+  fs.watch(certPath, (_eventType, filename) => {
+    if (filename === "cert.pem" || filename === "privkey.pem") {
+      console.log("証明書の変更を検知しました。サーバーを再起動します...");
+      setTimeout(startServer, 1000); // 少し待ってから再起動
+    }
+  });
+
+  startServer();
 } else {
   app.listen(process.env.PORT || "3000");
 }
